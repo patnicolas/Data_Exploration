0cbb61442815d94b8862c3218708b27a4e4b2cb3
PatNicolas <patricknicolas@Patricks-MacBook-Pro-M2.local>
2024-04-30 23:17:20 -0700

    Add Riemannian clustering to KMeans on Manifold

--- src/manifolds/kmeansonmanifold.py
+++ src/manifolds/kmeansonmanifold.py
@@ -4,33 +4,69 @@ __copyright__ = "Copyright 2023, 2024  All rights reserved."
 from geomstats.geometry.hypersphere import Hypersphere
 from geomstats.geometry.special_orthogonal import SpecialOrthogonal
 from typing import AnyStr, List
-from sklearn.cluster import KMeans
 import numpy as np
 from dataclasses import dataclass
+import geomstats.backend as gs
 
 @dataclass
 class KMeansResult:
     center: np.array
+    label: np.array
     predicted: np.array
 
     def __str__(self) -> AnyStr:
-        return f'Center: {self.center}\nPredicted: {self.predicted}'
+        return f'Center: {self.center}, Label: {self.label}, Predicted: {self.predicted}'
 
 
 class KMeansOnManifold(object):
-    def __init__(self, num_samples: int, num_clusters: int):
-        hypersphere = Hypersphere(dim=2, equip=False)
-        cluster = hypersphere.random_von_mises_fisher(kappa=20, n_samples=num_samples)
-        so3_lie = SpecialOrthogonal(3, equip=False)
-        self.clusters = [cluster @ so3_lie.random_uniform() for _ in range(num_clusters)]
-        self.data = np.concatenate(self.clusters)
+    def __init__(self, num_samples: int, num_clusters: int, random_gen: AnyStr = 'random_von_mises_fisher'):
+        """
+        Constructor for the evaluation of KMeans on Euclidean space and Riemann manifold (Hypersphere)
+        :param num_samples: Number of random samples
+        :type num_samples: int
+        :param num_clusters: Number of clusters used in KMeans
+        :type num_clusters: int
+        :param random_gen: Random generator identifier on the hypersphere
+        :type random_gen: AnyStr
+        """
+        self.hypersphere = Hypersphere(dim=2, equip=True)
+        match random_gen:
+            case 'random_von_mises_fisher':
+                cluster = self.hypersphere.random_von_mises_fisher(kappa=20, n_samples=num_samples)
+            case 'random_rand_riemann_normal':
+                cluster = self.hypersphere.random_riemannian_normal(n_samples=num_samples)
+            case 'random_uniform':
+                cluster = self.hypersphere.random_uniform(n_samples=num_samples)
+            case _:
+                cluster = self.hypersphere.random_uniform(n_samples=num_samples)
+        # Use the symmetric rotation Lie group in dimension 3
+        so3_lie_group = SpecialOrthogonal(3, equip=False)
+        # Generate the clusters
+        self.clusters = [cluster @ so3_lie_group.random_uniform() for _ in range(num_clusters)]
 
     def __str__(self) -> AnyStr:
         return '\n'.join([f'Cluster: {idx+1}\n{str(cluster)}' for idx, cluster in enumerate(self.clusters)])
 
-    def euclidean(self) -> List[KMeansResult]:
+    def euclidean_clustering(self) -> List[KMeansResult]:
+        from sklearn.cluster import KMeans
+
         kmeans = KMeans(n_clusters=len(self.clusters))
-        kmeans.fit(self.data)
-        predicted = kmeans.predict(self.data)
+        data = np.concatenate(self.clusters, axis=0)
+        kmeans.fit(data)
+        predictions = kmeans.predict(data)
         centers = kmeans.cluster_centers_
-        return [KMeansResult(c, y) for y, c in zip(predicted, centers)]
+        labels = kmeans.labels_
+        return [KMeansResult(center, label, prediction)
+                for center, label, prediction in zip(centers, labels, predictions)]
+
+    def riemannian_clustering(self) -> List[KMeansResult]:
+        from geomstats.learning.kmeans import RiemannianKMeans
+
+        kmeans = RiemannianKMeans(space=self.hypersphere, n_clusters=len(self.clusters))
+        data = gs.concatenate(self.clusters, axis =0)
+        kmeans.fit(data)
+        centers = kmeans.centroids_
+        labels = kmeans.labels_
+        predictions = kmeans.predict(data)
+        return [KMeansResult(center, label, prediction)
+                for center, label, prediction in zip(centers, labels, predictions)]
--- src/manifolds/test/test_kmeansonmanifold.py
+++ src/manifolds/test/test_kmeansonmanifold.py
@@ -15,8 +15,17 @@ class KMeansOnManifoldTest(unittest.TestCase):
         num_samples = 100
         num_clusters = 3
         kmeans = KMeansOnManifold(num_samples, num_clusters)
-        kmeans_result = kmeans.euclidean()
-        print('\n'.join([f'Cluster: {idx}\n{str(cluster_result)}' for idx, cluster_result in enumerate(kmeans_result)]))
+        kmeans_result = kmeans.euclidean_clustering()
+        print('\nEuclidean --------')
+        print('\n'.join([f'Cluster #{idx+1}\n{str(cluster_result)}' for idx, cluster_result in enumerate(kmeans_result)]))
+
+    def test_riemannian(self):
+        num_samples = 100
+        num_clusters = 3
+        kmeans = KMeansOnManifold(num_samples, num_clusters)
+        kmeans_result = kmeans.riemannian_clustering()
+        print('\nRiemannian --------')
+        print('\n'.join([f'Cluster #{idx+1}\n{str(cluster_result)}' for idx, cluster_result in enumerate(kmeans_result)]))
 
 
 if __name__ == '__main__':
