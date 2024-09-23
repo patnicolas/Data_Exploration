Fix bug for computation of Lie bracket

src/Lie/Lie_SO3_group.py
@@ -46,12 +46,18 @@ class LieSO3Group(object):
         @param base_point: Base point vector on the manifold (Identity [0, 0, 0] if not defined)
         @type base_point: Numpy array
         """
-        assert tgt_vector.size == 9, f'Rotation matrix size {tgt_vector.size} should be 9'
+        assert tgt_vector.size == 9, f'Tangent vector size {tgt_vector.size} should be 9'
+        assert base_point.size == 9, f'Base point size {tgt_vector.size} should be 9'
 
         self.tangent_vec = gs.array(tgt_vector)
         # Exp. a left-invariant vector field from a base point
         self.group_element = LieSO3Group.lie_group.exp(self.tangent_vec, base_point)
         self.base_point = base_point
+
+    def validate(self) -> np.array:
+        det = np.dot(self.group_element, self.group_element.T)
+        diff = np.abs(det - LieSO3Group.identity)
+        return diff
 
     @classmethod
     def build(cls, tgt_vector: List[float], base_point: List[float] = None) -> Self:
@@ -126,29 +132,46 @@ class LieSO3Group(object):
         assert len(_tgt_vector) == 9, f'Rotation matrix size {len(_tgt_vector)} should be 9'
 
         np_tgt_vector = np.reshape(_tgt_vector, (3, 3))
-        tangent_vec = gs.array(np_tgt_vector)
-        return LieSO3Group.lie_group.lie_bracket(self.group_element, tangent_vec)
+        return np.dot(self.tangent_vec, np_tgt_vector) - np.dot(np_tgt_vector, self.tangent_vec)
 
-    def visualize(self, title: AnyStr, notation_indices: int) -> NoReturn:
+    def visualize(self, title: AnyStr, notation_index: int = 0) -> NoReturn:
+        """
+        Visualize this element on SO3 Lie group. The element is defined through the exponential map
+        of the tangent vector + base point  (if not identity)
+        @param title: Title for the plot
+        @type title: str
+        @param notation_index: Indices to label the base point on the plot
+        @type notation_index: int
+        """
         so3_point = SO3Point(self.group_element, self.base_point, title)
-        LieSO3Group.visualize_all([so3_point], notation_indices)
+        LieSO3Group.visualize_all([so3_point], notation_index)
 
     @staticmethod
-    def visualize_all(so3_points: List[SO3Point], notation: int) -> NoReturn:
+    def visualize_all(so3_points: List[SO3Point], notation_index: int) -> NoReturn:
+        """
+        Visualize (plot) multiple SO3 points
+        @param so3_points: List of SO3 points
+        @type so3_points: List[SO3Point]
+        @param notation_index: Index used to add notation for base point {1 first plot, 2 second plot, 3 all plot)
+        @type notation_index: int
+        """
         import matplotlib.pyplot as plt
 
         fig = plt.figure(figsize=(12, 12))
-        if len(so3_points) == 1:
-            ax = fig.add_subplot(111, projection="3d")
-            LieSO3Group.__visualize_one(so3_points[0], ax)
-        else:
-            ax1 = fig.add_subplot(121, projection="3d")
-            is_notation = notation == 1 or notation == 3
-            LieSO3Group.__visualize_one(so3_points[0], ax1, is_notation)
-            ax2 = fig.add_subplot(122, projection="3d")
-            is_notation = notation == 2 or notation == 3
-            LieSO3Group.__visualize_one(so3_points[1], ax2, is_notation)
 
+        match len(so3_points):
+            case 1:       # If we display on one SO3 point
+                ax = fig.add_subplot(111, projection="3d")
+                LieSO3Group.__visualize_one(so3_points[0], ax, notation_index > 0)
+            case 2:       # Visualize two data points
+                ax1 = fig.add_subplot(121, projection="3d")
+                is_notation = notation_index == 1 or notation_index == 3
+                LieSO3Group.__visualize_one(so3_points[0], ax1, is_notation)
+                ax2 = fig.add_subplot(122, projection="3d")
+                is_notation = notation_index == 2 or notation_index == 3
+                LieSO3Group.__visualize_one(so3_points[1], ax2, is_notation)
+            case _:
+                raise Exception(f'Number of SO3 point to display {len(so3_points)} should be {1, 2}')
         plt.legend()
         plt.tight_layout()
         plt.show()

src/Lie/test/test_Lie_SO3_group.py
@@ -1,49 +1,62 @@
+import math
 import unittest
 
 from Lie.Lie_SO3_group import LieSO3Group
 from Lie.Lie_SO3_group import SO3Point
+import numpy as np
+from typing import AnyStr, List
 
 
 class LieSO3GroupTest(unittest.TestCase):
+    so3_rot_tgt_vectx = [1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0]
+    so3_rot_tgt_vecty = [0.0, 0.0, 1.0, 0.0, 1.0, 0.0, -1.0, 0.0, 0.0]
+    so3_rot_tgt_vectz = [0.0, -1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0]
+
 
     @unittest.skip('Ignored')
     def test_init(self):
-        so3_tangent_vec = [0.4, 0.3, 0.8, 0.2, 0.4, 0.1, 0.1, 0.2, 0.6]
+        so3_tangent_vec = LieSO3GroupTest.__create_tangent_vec('x', math.pi/2, LieSO3Group.identity)
+        so3_tangent_vec = [0.0, 0.0, 0.0, 0.0, -1.0, -1.0, 0.0, 1.0, -1.0]
+
+        det = np.linalg.trace(np.reshape(so3_tangent_vec, (3, 3)))
         so3_group = LieSO3Group.build(so3_tangent_vec)
         print(str(so3_group))
         self.assertTrue(so3_group.group_element.shape == (3, 3))
+
 
     @unittest.skip('Ignored')
     def test_init_2(self):
         import numpy as np
 
-        so3_tangent_vec = [0.4, 0.3, 0.8, 0.2, 0.4, 0.1, 0.1, 0.2, 0.6]
+        so3_tangent_vec = [1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0]
         so3_group = LieSO3Group.build(so3_tangent_vec)
-        print(str(so3_group))
         so3_point1 = SO3Point(
             group_element=so3_group.group_element,
             base_point=LieSO3Group.identity,
-            descriptor='SO3 point elements\n[0.4 0.3 0.8]\n[0.2 0.4 0.1]\n[0.1 0.2 0.6]\n@Identity')
+            descriptor='SO3 point from tangent vector\n[1 0  0]\n[0 0 -1]\n[0 1  0]\nBase point: Identity')
 
         self.assertTrue(so3_group.group_element.shape == (3, 3))
 
-        base_point = [-0.4, 0.5, -0.3, 0.0, 1.0, -0.6, 0.2, 0.1, 0.0]
+        base_point = [0.0, -1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0]
         so3_group2 = LieSO3Group.build(so3_tangent_vec, base_point)
         so3_point2 = SO3Point(
             group_element=so3_group2.group_element,
             base_point=np.reshape(base_point, (3, 3)),
-            descriptor='Same SO3 point elements\n@ [-0.4  0.5 -0.3]\n[ 0.0  1.0 -0.6]\n[ 0.2  0.1  0.0]')
-        LieSO3Group.visualize_all([so3_point1, so3_point2])
+            descriptor='      Same SO3 point\nBase point:\n[0 -1  0]\n[0  0  0]\n[0  0  1]')
+        print(str(so3_group2))
+        LieSO3Group.visualize_all([so3_point1, so3_point2], 3)
+
 
     @unittest.skip('Ignored')
     def test_inverse(self):
         # Original SO3 rotation matrix
-        so3_tangent_vec = [0.4, 0.3, 0.8, 0.2, 0.4, 0.1, 0.1, 0.2, 0.6]
+        so3_tangent_vec = [1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0]
         so3_group = LieSO3Group.build(so3_tangent_vec)
         so3_point = SO3Point(
             group_element=so3_group.group_element,
             base_point=LieSO3Group.identity,
-            descriptor='SO3 point:\n[0.4 0.3 0.8]\n[0.2 0.4 0.1]\n[0.1 0.2 0.6]\n@ Identity')
+            descriptor='SO3 point from tangent vector\n[1 0  0]\n[0 0 -1]\n[0 1  0]\nBase point: Identity')
+
         print(str(so3_group))
 
         # Inverse SO3 rotation matrix
@@ -51,31 +64,30 @@ class LieSO3GroupTest(unittest.TestCase):
         inv_so3_point = SO3Point(
             group_element=so3_inv_group.group_element,
             base_point=LieSO3Group.identity,
-            descriptor='SO3 inverse point:\n[-0.4 -0.3 -0.8]\n[-0.2 -0.4 -0.1]\n[-0.1 -0.2 -0.6]')
+            descriptor='SO3 inverse point')
         print(f'SO3 Inverse point:{so3_inv_group}')
         self.assertTrue(so3_inv_group.group_element.shape == (3, 3))
-
         # Visualization
-        LieSO3Group.visualize_all([so3_point, inv_so3_point], 1)
+        LieSO3Group.visualize_all([so3_point, inv_so3_point], 2)
 
 
     @unittest.skip('Ignored')
     def test_product_1(self):
-        # First SO3 rotation matrix
-        so3_tangent_vec = [0.4, 0.3, 0.8, 0.2, 0.4, 0.1, 0.1, 0.2, 0.6]
+        # First SO3 rotation matrix 90 degree along x axis
+        so3_tangent_vec = [1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0]
         so3_group = LieSO3Group.build(tgt_vector=so3_tangent_vec)
         so3_point = SO3Point(
             group_element=so3_group.group_element,
             base_point=LieSO3Group.identity,
-            descriptor='SO3 point:\n[0.4 0.3 0.8]\n[0.2 0.4 0.1]\n[0.1 0.2 0.6]\n@ Identity')
+            descriptor='SO3 point from tangent vector\n[1 0  0]\n[0 0 -1]\n[0 1  0]\nBase point: Identity')
 
         # Second SO3 rotation matrix
-        so3_tangent_vec2 = [-x for x in so3_tangent_vec]
+        so3_tangent_vec2 = [0.0, -1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0]
         so3_group2 = LieSO3Group.build(tgt_vector=so3_tangent_vec2)
         so3_point2 = SO3Point(
             group_element=so3_group2.group_element,
             base_point=LieSO3Group.identity,
-            descriptor='SO3 inverse point:\n[-0.4 -0.3 -0.8]\n[-0.2 -0.4 -0.1]\n[-0.1 -0.2 -0.6]\n@ Identity')
+            descriptor='SO3 point from tangent vector\n[0 -1 0]\n[1  0 0]\n[0  0 1]\nBase point: Identity')
 
         # Composition of two SO3 rotation matrices
         so3_group_product = so3_group.product(so3_group2)
@@ -83,27 +95,26 @@ class LieSO3GroupTest(unittest.TestCase):
         print(f'\nSO3 Product:{so3_group_product}')
 
         # Visualization
-        LieSO3Group.visualize_all([so3_point, so3_point2], 1)
-        so3_group_product.visualize('Composition of 3D rotations', 1)
+        LieSO3Group.visualize_all([so3_point, so3_point2], 0)
+        so3_group_product.visualize('Composition of two SO3 matrices\nRotation along X with Z axis', 0)
 
-
-
+    @unittest.skip('Ignored')
     def test_product_2(self):
         # First SO3 rotation matrix
-        so3_tangent_vec = [0.4, 0.3, 0.8, 0.2, 0.4, 0.1, 0.1, 0.2, 0.6]
+        so3_tangent_vec = [1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0]
         so3_group = LieSO3Group.build(tgt_vector=so3_tangent_vec)
         so3_point = SO3Point(
             group_element=so3_group.group_element,
             base_point=LieSO3Group.identity,
-            descriptor='SO3 point:\n[0.4 0.3 0.8]\n[0.2 0.4 0.1]\n[0.1 0.2 0.6]\n@ Identity')
+            descriptor='SO3 point from tangent vector\n[1 0  0]\n[0 0 -1]\n[0 1  0]\nBase point: Identity')
 
         # Second SO3 rotation matrix
-        so3_tangent_vec2 = [-0.5]*len(so3_tangent_vec)
-        so3_group2 = LieSO3Group.build(tgt_vector=so3_tangent_vec2)
+        identity_vec2 = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
+        so3_group2 = LieSO3Group.build(tgt_vector=identity_vec2)
         so3_point2 = SO3Point(
             group_element=so3_group2.group_element,
             base_point=LieSO3Group.identity,
-            descriptor='SO3 point:\n[-0.5 -0.5 -0.5]\n[-0.5 -0.5 -0.5]\n[-0.5 -0.5 -0.5]\n@ Identity')
+            descriptor='SO3 point from identity matrix\nBase point: Identity')
 
         # Composition of two SO3 rotation matrices
         so3_group_product = so3_group.product(so3_group2)
@@ -111,13 +122,13 @@ class LieSO3GroupTest(unittest.TestCase):
         print(f'\nSO3 points composition:{so3_group_product}')
 
         # Visualization
-        LieSO3Group.visualize_all([so3_point, so3_point2], 1)
-        so3_group_product.visualize('Composition of 3D rotations', 0)
+        LieSO3Group.visualize_all([so3_point, so3_point2], 0)
+        so3_group_product.visualize('Composition of two SO3 matrices\n90 degree rotation with identity', 0)
 
     @unittest.skip('Ignored')
     def test_product_3(self):
         # First SO3 rotation matrix
-        so3_tangent_vec = [0.4, 0.3, 0.8, 0.2, 0.4, 0.1, 0.1, 0.2, 0.6]
+        so3_tangent_vec = [1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0]
         so3_group = LieSO3Group.build(tgt_vector=so3_tangent_vec)
         so3_point = SO3Point(
             group_element=so3_group.group_element,
@@ -137,65 +148,145 @@ class LieSO3GroupTest(unittest.TestCase):
         self.assertTrue(so3_group_product.group_element.shape == (3, 3))
         print(f'\nSO3 Product:{so3_group_product}')
 
-        LieSO3Group.visualize_all([so3_point, so3_point2])
+        LieSO3Group.visualize_all([so3_point, so3_point2], 0)
         so3_group_product.visualize('Composition of 3D rotations')
 
 
     @unittest.skip('Ignored')
     def test_algebra(self):
-        import random
-        so3_tangent_vec = [random.randint(-10, 10)*0.1 for _ in range(9)]
+        # First SO3 rotation matrix 90 degree along x axis
+        so3_tangent_vec = [1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0]
+
         so3_group = LieSO3Group.build(tgt_vector=so3_tangent_vec)
         print(f'SO3 Rotation matrix: {so3_group}')
         lie_algebra = so3_group.lie_algebra()
         assert lie_algebra.size == len(so3_tangent_vec)
         print(f'\nLie algebra:\n{lie_algebra}')
 
+
     @unittest.skip('Ignored')
     def test_algebra2(self):
-        import random
-        so3_tangent_vec = [random.randint(-10, 10) * 0.1 for _ in range(9)]
-        base_point = [-0.4, 0.5, -0.3, 0.0, 1.0, -0.6, 0.2, 0.1, 0.0]
+        # First SO3 rotation matrix 90 degree along x axis
+        so3_tangent_vec = [1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0]
+
+        # Base point is SO3 rotation matrix 90 degree along y axis
+        base_point = [0.0, 0.0, 1.0, 0.0, 1.0, 0.0, -1.0, 0.0, 0.0]
         so3_group = LieSO3Group.build(tgt_vector=so3_tangent_vec, base_point=base_point)
-        print(f'SO3 Rotation matrix: {so3_group}')
+        print(f'SO3 point:\n{so3_group}')
         lie_algebra = so3_group.lie_algebra()
-        assert lie_algebra.size == len(so3_tangent_vec)
         print(f'\nLie algebra:\n{lie_algebra}')
+
 
     @unittest.skip('Ignored')
     def test_projection(self):
-        import random
-        so3_tangent_vec = [random.randint(-10, 10) * 0.1 for _ in range(9)]
-        base_point = [-0.4, 0.5, -0.3, 0.0, 1.0, -0.6, 0.2, 0.1, 0.0]
+        # First SO3 rotation matrix 90 degree along x axis
+        so3_tangent_vec = [1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0]
+
+        so3_group = LieSO3Group.build(tgt_vector=so3_tangent_vec)
+        projected = so3_group.projection()
+        print(f'\nProjected point with identity:\n{projected.group_element}')
+
+        # Base point is SO3 rotation matrix 90 degree along y axis
+        base_point = [0.0, 0.0, 1.0, 0.0, 1.0, 0.0, -1.0, 0.0, 0.0]
         so3_group = LieSO3Group.build(tgt_vector=so3_tangent_vec, base_point=base_point)
         projected = so3_group.projection()
-        print(f'\nProjected point:\n{projected.group_element}')
+        print(f'\nProjected point with base point\n{so3_group.tangent_vec}:\n{projected.group_element}')
+
+
+    def test_bracket(self):
+        # First SO3 rotation matrix 90 degree along x axis
+        so3_tangent_vec = [1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0]
+        so3_group = LieSO3Group.build(tgt_vector=so3_tangent_vec)
+        print(f'\nTangent vector:\n{so3_group.tangent_vec}')
+        np.set_printoptions(precision=3)
+        print(f'\nSO3 point\n{so3_group.group_element}')
+        bracket = so3_group.bracket(so3_tangent_vec)
+        print(f'\nBracket [x,x]:\n{bracket}')
 
 
     @unittest.skip('Ignored')
-    def test_bracket(self):
-        import numpy as np
+    def test_bracket2(self):
+        # First SO3 rotation matrix 90 degree along x axis
+        so3_tangent_vec = [1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0]
+        so3_groupA = LieSO3Group.build(tgt_vector=so3_tangent_vec)
+        print(f'\n{so3_groupA.group_element}')
 
-        a = np.eye(3)
-
-        so3_tangent_vec = [0.4, 0.3, 0.8, 0.2, 0.4, 0.1, 0.1, 0.2, 0.6]
-        so3_group = LieSO3Group.build(tgt_vector=so3_tangent_vec, base_point=LieSO3Group.identity)
-        print(f'\n{so3_group.group_element}')
-        other_tgt_vec = [0.1, -0.3, 0.5, 0.1, 0.0, -0.1, 0.6, -0.3, 0.7]
-        so3_group2 = LieSO3Group.build(tgt_vector=other_tgt_vec, base_point=LieSO3Group.identity)
-        print(f'\n{so3_group2.group_element}')
-        """
-        first_term = Matrices.mul(inverse_base_point, tangent_vector_b)
-        first_term = Matrices.mul(tangent_vector_a, first_term)
-
-        second_term = Matrices.mul(inverse_base_point, tangent_vector_a)
-        second_term = Matrices.mul(tangent_vector_b, second_term)
-        """
-        x = np.dot(so3_group.group_element, so3_group2.group_element)
-        print(f'A.B\n{x}')
-        y = np.dot(so3_group2.group_element, so3_group.group_element)
-        print(f'B.A\n{y}')
-        print(f'Manual bracket\n{(x - y)}')
-        bracket = so3_group.bracket(other_tgt_vec)
+        # Second SO3 rotation matrix 90 degree along y axis
+        other_tgt_vec = [0.0, 0.0, 1.0, 0.0, 1.0, 0.0, -1.0, 0.0, 0.0]
+        bracket = so3_groupA.bracket(other_tgt_vec)
         print(f'\nBracket:\n{bracket}')
+        so3_groupB = LieSO3Group.build(tgt_vector=other_tgt_vec)
+
+        so3_pointA = SO3Point(
+            group_element = so3_groupA.tangent_vec,
+            base_point = LieSO3Group.identity,
+            descriptor='SO3 point from tangent vector\n[1 0  0]\n[0 0 -1]\n[0 1  0]\nBase point: Identity')
+        print(f'SO3 point A:\n{so3_pointA.group_element}')
+        so3_pointB = SO3Point(
+            group_element = so3_groupB.tangent_vec,
+            base_point = LieSO3Group.identity,
+            descriptor='SO3 point from tangent vector\n[ 0 0 1]\n[ 0 1 0]\n[-1 0  0]\nBase point: Identity')
+        print(f'SO3 point B:\n{so3_pointB.group_element}')
+        LieSO3Group.visualize_all([so3_pointA, so3_pointB], 0)
+
+        so3_point = SO3Point(
+            group_element=bracket,
+            base_point=LieSO3Group.identity,
+            descriptor='Lie Bracket')
+        LieSO3Group.visualize_all([so3_point], 0)
+
+
+    @unittest.skip('Ignored')
+    def test_visualize_geomstats(self):
+        import geomstats.backend as gs
+        import matplotlib.pyplot as plt
+        from geomstats.geometry.special_orthogonal import SpecialOrthogonal
+        import geomstats.visualization as visualization
+        n_steps = 10
+
+        so3_group = SpecialOrthogonal(n=3, point_type="vector")
+
+        initial_point = so3_group.identity
+        initial_tangent_vec = gs.array([0.5, 0.5, 0.8])
+        geodesic = so3_group.metric.geodesic(
+            initial_point=initial_point, initial_tangent_vec=initial_tangent_vec
+        )
+
+        t = gs.linspace(0.0, 1.0, n_steps)
+
+        points = geodesic(t)
+        visualization.plot(points, space="SO3_GROUP")
+        plt.show()
+
+    """ --------------------------  Private helper static methods -------------- """
+
+    @staticmethod
+    def __create_rotation_matrix(rotation_axis: AnyStr, theta: float) -> np.array:
+        import math
+        match rotation_axis:
+            case 'x':
+                return np.array(
+                    [[1.0, 0.0, 0.0],
+                     [0.0, math.cos(theta), -math.sin(theta)],
+                     [0.0, math.sin(theta), math.cos(theta)]]
+                )
+            case 'y':
+                return np.array(
+                    [[math.cos(theta), 0.0, math.sin(theta)],
+                     [0.0, 1.0, 0.0],
+                     [-math.sin(theta), 0.0, math.cos(theta)]]
+                )
+            case 'z':
+                return np.array(
+                    [[math.cos(theta), -math.sin(theta), 0.0],
+                     [math.sin(theta), math.cos(theta), 0.0],
+                     [0.0, 0.0, 1.0]]
+                )
+            case _:
+                raise Exception(f'Rotation axis {rotation_axis} is undefined')
+
+    @staticmethod
+    def __create_tangent_vec(rotation_axis: AnyStr, theta: float, base_point) -> List[float]:
+        so3_matrix = LieSO3GroupTest.__create_rotation_matrix(rotation_axis, theta)
+        return list(LieSO3Group.lie_group.log(so3_matrix, base_point))
 